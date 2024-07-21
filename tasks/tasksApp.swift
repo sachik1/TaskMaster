552e2958f0a63ab217280b360682ab274279cd73
sachik1 <sachikelkar@gmail.com>
2024-07-21 14:32:56 -0700

    Initial Commit

---
+++ tasks/tasksApp.swift
@@ -0,0 +1,17 @@
+//
+//  tasksApp.swift
+//  tasks
+//
+//  Created by Sachi Kelkar on 7/21/24.
+//
+
+import SwiftUI
+
+@main
+struct tasksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
